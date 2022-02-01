d549770d40a52418cd27d620e2a57c5aa20d24a6
Soumithkumar12 <94587623+Soumithkumar12@users.noreply.github.com>
2022-02-01 10:08:44 +0000

    p3 original

---
+++ p3original.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int input ()
+{
+  int a;
+  printf("enter value");
+  scanf("%d",&a);
+  return a;
+}
+int add (int a,int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+void output(int a,int b,int c)
+{
+  printf("%d+%d=%d",a,b,c);
+}
+int main()
+{
+  int a,b,c;
+  a=input();
+  b=input();
+  c=add(a,b);
+  output(a,b,c);
+  return 0;
+
+}
